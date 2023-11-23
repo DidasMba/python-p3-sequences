a005a37455d52d02c3028398587f94e9a0cf7816
DidasMba <daniel.mbarushi@gmail.com>
2023-11-23 20:32:21 +0300

    fibonaccisequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
+# lib/sequences.py
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1]
+
+    while len(fibonacci_sequence) < length:
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
+
+    print(fibonacci_sequence[:length])
+
+# Uncomment the line below if you want to test the function with print_fibonacci(9)
+# print_fibonacci(9)
+
